f9505a0c32c35eb50f93ef9e0839f50978d46e07
tejareddysabbella-source <tejareddysabbella@gmail.com>
2026-04-01 09:18:22 +0530

    UC4: Maintain ordered train consist using LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
 public class TrainApp {
 
@@ -7,15 +6,27 @@ public class TrainApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        Set<String> bogieIds = new HashSet<>();
 
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101"); // duplicate
-        bogieIds.add("BG102"); // duplicate
+        LinkedList<String> trainConsist = new LinkedList<>();
 
-        System.out.println("\nUnique Bogie IDs:");
-        System.out.println(bogieIds);
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        System.out.println("\nInitial Train Consist:");
+        System.out.println(trainConsist);
+
+        trainConsist.add(2, "Pantry");
+
+        System.out.println("\nAfter adding Pantry Car at position 2:");
+        System.out.println(trainConsist);
+
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(trainConsist);
     }
 }
